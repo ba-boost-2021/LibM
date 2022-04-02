ceb46edeb52ba38123979f99dc32ab9a9a6f6b3b
MUSTAFA KAAN ÇALIŞKAN <mkaancaliskan@gmail.com>
2022-04-02 17:31:57 +0300

    New Book  added

--- LibM.Contracts/Books/BookViewDto.cs
+++ LibM.Contracts/Books/BookViewDto.cs
@@ -8,4 +8,14 @@
         public string Type { get; set; }
         public bool Available { get; set; }
     }
+
+    public class NewBookDto
+    {
+        public string Name { get; set; }
+        public Guid AuthorId { get; set; }
+
+        public Guid TypeId { get; set; }
+        public bool Available { get; set; }
+        public int PageCount { get; set; }
+    }
 }
