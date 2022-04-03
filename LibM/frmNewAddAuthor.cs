441d50dc3dbd7f347169eadac7077586c8a65fcf
burakyuz1 <burakyuzz@gmail.com>
2022-04-03 13:15:10 +0300

    Süreçler tamamlandı

---
+++ LibM/frmNewAddAuthor.cs
@@ -0,0 +1,43 @@
+﻿using LibM.Contracts.NewAddAuthor;
+using LibM.Services.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibM
+{
+    public partial class frmNewAddAuthor : Form
+    {
+        private readonly AuthorRepository authorRepository;
+        public frmNewAddAuthor()
+        {
+            InitializeComponent();
+            authorRepository = new AuthorRepository();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = authorRepository.CreateNewAuthor (new NewAddAuthorViewDto
+            {
+                FirstName = txtFirstName.Text,
+                LastName = txtLastName.Text,
+               
+            });
+            if (result)
+            {
+                MessageBox.Show("İşlem Başarılı.", "Başarılı", MessageBoxButtons.OK);
+            }
+            else
+            {
+                MessageBox.Show("Tekrar Deneyiniz.", "Başarısız İşlem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
